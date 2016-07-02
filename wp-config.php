195fa1bb56f90923cc83ea4e9c3e653f77a79f97
root <root@ip-192-168-0-118.ap-southeast-1.compute.internal>
2016-07-02 14:01:26 +0000

    removed access keys

--- wp-config.php
+++ wp-config.php
@@ -58,8 +58,7 @@ define('NONCE_SALT',       'L?$s*1EfZ/3JpP>ZaETQd+Z0D$|sniayaRhzZTx$9.CH^oV:-{y3
 /**#@-*/
 
 /** Define AWS Access Key **/
-define( 'AWS_ACCESS_KEY_ID', 'AKIAJYS335O6DXLTFNWA' );
-define( 'AWS_SECRET_ACCESS_KEY', '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' );
+
 /* Define REDIS node endpoint */
 define('WP_REDIS_HOST', 'cdc.ywsccw.0001.apse1.cache.amazonaws.com');
 
